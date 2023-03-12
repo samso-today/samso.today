38463b6555cbb3879118cf2e105114d4bccf7d85
hyeongseok.so <hyeongseok.so@linecorp.com>
2023-03-13 00:55:30 +0900

    chore: add plugins that transforms markdown to graphql, and get picture from graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,29 @@ module.exports = {
     description: '오늘의 쌤소',
     siteUrl: `https://samso.blog.not.decided`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/posts`,
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 800,
+            }
+          }
+        ]
+      }
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-image',
+  ],
 }
