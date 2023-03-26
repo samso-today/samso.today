3173f34f8eb1018ebc801f54c35b23ec4be3e9d1
hyeongseok.so <hyeongseok.so@linecorp.com>
2023-03-26 09:57:26 +0900

    feat: add About page with blank

---
+++ src/components/templates/About/About.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+import Header from "../../organisms/Header/Header"
+import Footer from "../../organisms/Footer/Footer"
+import "./About.css"
+
+export default function About() {
+    return (
+        <div class="about">
+            <Header />
+            <main>
+                <div class="main-wrapper">
+                    <h1 style={{ margin: "0", paddingTop: "20px" }}>To Be Continued</h1>
+                </div>
+            </main>
+            <Footer />
+        </div >
+    )
+}
---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import About from "../components/templates/About/About"
+
+export default function AboutPage() {
+    return (
+        <About />
+    )
+}
+
+export const Head = () => <title>About</title>
