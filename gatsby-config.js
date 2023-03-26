d5fb04e4cdaa0266f0004049e34a504be9d5fc8c
hyeongseok.so <hyeongseok.so@linecorp.com>
2023-03-26 17:15:42 +0900

    fix: re-write description

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
   siteMetadata: {
     title: 'samso.today',
     author: 'Sam So',
-    description: '귀한 손님이 누추한 곳까지 찾아오시다니, 환영합니다!',
+    description: '간헐적 업로드 예정, 쌤소의 하루',
     image: `/thumbnail.png`,
     siteUrl: `https://samso.today`,
   },
